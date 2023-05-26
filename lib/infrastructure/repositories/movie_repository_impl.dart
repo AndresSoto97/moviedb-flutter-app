4c23fc98126d9a66c0c36122c23e229ffb827a2d
Snowcom415 <diegoasg5@gmail.com>
2023-05-26 16:02:08 -0600

    now playing movies list loaded

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:cinemapedia/domain/datasurces/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl implements MovieRepository {
+  final MovieDataSource dataSource;
+
+  MovieRepositoryImpl(this.dataSource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return dataSource.getNowPlaying(page: page);
+  }
+}
